84a2f73f44f8a588b34223920feae19f4cc3a04d
Tehnikumi Opilane <opilane@tptlive.ee>
2018-10-01 09:44:25 +0300

    asdsadsdasda

---
+++ WeatherApp/WeatherApp.Core/Core.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core
+{
+    public class Core
+    {
+        public static async Task<Weather> GetWeather(string zipCode)
+        {
+            string key = "1dad50e251b03e9137b8b650a95bc9ff";
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?q=Tallinn&units=metric&appid="+ key;
+
+            dynamic results = await DataService.GetDataFromService(queryString).ConfigureAwait(false);
+            var weather = new Weather();
+            weather.Temperature = (string)results["main"]["temp"] + " C";
+            return weather;
+        }
+    }
+}
---
+++ WeatherApp/WeatherApp/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace WeatherApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var button = FindViewById<Button>(Resource.Id.button1);
+            
+
+            button.Click += Button_Click;
+
+            
+        }
+        private async void Button_Click(object sender, System.EventArgs e)
+        {
+            var textview = FindViewById<TextView>(Resource.Id.textView1);
+            var weather = await Core.Core.GetWeather("asd");
+            textview.Text = weather.Temperature;
+            
+        }
+    }
+}
